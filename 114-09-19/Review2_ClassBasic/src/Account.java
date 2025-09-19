c36fbf7ce2567c9e101c21789600b3444fccf5c1
a113221091-eng <a113221091@mail.shu.edu.tw>
2025-09-19 15:05:36 +0800

    114-09-19

---
+++ 114-09-19/Review2_ClassBasic/src/Account.java
@@ -0,0 +1,31 @@
+public class Account {
+    // 帳戶號碼
+    private String accountNumber;
+    // 帳戶餘額
+    private double balance;
+
+    // 建構子，初始化帳戶號碼與初始餘額
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    // 取得帳戶號碼
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    // 取得帳戶餘額
+    public double getBalance() {
+        return balance;
+    }
+
+    // 存款方法，金額必須大於0
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            throw new IllegalArgumentException("存款金額必須為正數");
+        }
+    }
+}
